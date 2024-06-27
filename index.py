2ef54f99f4d5e9c1c60dec3e5b88ddc099e6d9d1
GVAmaresh <eshama944@gmail.com>
2024-06-28 01:35:34 +0530

    ini

--- index.py
+++ index.py
@@ -5,6 +5,11 @@ from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 from container import process_models, compare_text
 import pandas as pd
+import logging
+import json
+from pydantic import BaseModel
+
+logging.basicConfig(level=logging.INFO)
 app = FastAPI()
 
 app.add_middleware(
@@ -14,33 +19,62 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+class ReviewRequest(BaseModel):
+    review: List[str]
+
+class CompareRequest(BaseModel):
+    texts: List[str]
+    text: str
 
 @app.post("/api/v1/review")
-def get_review( text_arr: List[str] ):
+async def get_review(request):
     try:
-        if not text_arr: 
-            return {"status": 422, "message": "Invalid Input" }
+        print(request)
+        body = request.model_dump_json()
+        # return JSONResponse({"response": body})
+        if isinstance(body, bytes):
+            body = json.loads(body.decode('utf-8'))
+        else:
+            body = json.loads(body)
+        review_text = body.get("review")
+        if not review_text: 
+            return JSONResponse({"status": 422, "message": "Invalid Input" }, status_code=422)
             
-        output = [process_models(text) for text in text_arr]
+        output = [process_models(text) for text in review_text]
         if not output:
-            return {"status": 500,"message": "Data Couldn't saved", "data": {}}
-        return {"status": 200,"message": "Data saved Successfully", "data": output}
+            return JSONResponse({"status": 500,"message": "Data Couldn't saved", "data": {}}, status_code=500)
+        return JSONResponse({"status": 200,"message": "Data saved Successfully", "data": output}, status_code=200)
     except Exception as e:
-        return {"status": 500,"message": str(e)}
+        return JSONResponse({"status": 500,"message": str(e)}, status_code=500)
 
 @app.post("/api/v1/compare_text")
-def compare(texts: List[str], text:str):
+def compare(request:CompareRequest):
     try:
-        if not texts: 
-            return {"status": 422, "message": "Invalid Input" }
-        if not text: 
-            return {"status": 422, "message": "Invalid Input" }
+        print(request)
+        if not request.texts and not request.text:
+            body = request.model_dump_json()
+            if isinstance(body, bytes):
+                body = json.loads(body.decode('utf-8'))
+            else:
+                body = json.loads(body)
+            texts = body.get("texts")
+            text = body.get("text")
+        else:
+            texts = request.texts
+            text = request.text
+        if not texts or not text: 
+            return JSONResponse({"status": 422, "message": "Invalid Input"})
+
         output = compare_text(texts, text)
         if not output:
-            return {"status": 500,"message": "Data Couldn't saved", "data": {}}
-        return {"status": 200,"message": "Data saved Successfully", "data": output}
-    except Exception as e:
-        return {"status": 500,"message": str(e)}
+            return JSONResponse({"status": 500, "message": "No matching text found", "data": {}})
 
+        return JSONResponse({"status": 200, "message": "Matching text found", "data": output})
+    except Exception as e:
+        return JSONResponse({"status": 500, "message": str(e)})
+
+# get_review(
+#     ["All around lovely brunch spot! Cute and quaint decor, very friendly and accommodating host and wait staff, delicious coffee, BYOB and endless food options. It was very difficult to choose one thing, but the combinations are unique and flavorful- far from routine. The meals are reasonably portioned and packed with flavor. I appreciate the time out into each dish as opposed to hugeee portions of hastily made eggs or pancakes. There are so many elements to the dishes. I had the gypsy eggs! Will have took back to try other options! Expect to wait to be seated and don't go with a very large group as seating will be difficult- in my mind it's worth the wait and the wait shows that many others feel the same! Have heard from multiple sources that this was a spot not to miss and I 100% concur. Thank you for great food, friendly atmosphere and BYOB"]
+# )
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=5000)
--- models.py
+++ models.py
@@ -3,9 +3,11 @@
 # --------------------------------------------------------------------------
 from happytransformer import HappyTextToText, TTSettings
 import torch
+import nltk
+nltk.download('punkt')
+from fastapi.responses import JSONResponse
 
 happy_tt = HappyTextToText("T5", "vennify/t5-base-grammar-correction")
-
 if torch.cuda.is_available():
     happy_tt.model.to("cuda")
     happy_tt.tokenizer.to("cuda")
@@ -14,11 +16,15 @@ args = TTSettings(num_beams=5, min_length=1)
 
 
 def grammer_correction(text):
+    happy_tt = HappyTextToText("T5", "vennify/t5-base-grammar-correction")
+    args = TTSettings(num_beams=5, min_length=1)
+    print("HERE")
     result = happy_tt.generate_text(f"grammar: {text}", args=args)
-    print(result.text)
+    print("HERE2")
+    return result.text
 
-
-# grammer_correction("He are moving here.")
+output = grammer_correction("He are moving here.")
+# print(output)
 
 
 # --------------------------------------------------------------------------
@@ -26,12 +32,12 @@ def grammer_correction(text):
 # --------------------------------------------------------------------------
 from transformers import T5Tokenizer, T5ForConditionalGeneration
 
-checkpoint = "unikei/t5-base-split-and-rephrase"
-tokenizer = T5Tokenizer.from_pretrained(checkpoint)
-model = T5ForConditionalGeneration.from_pretrained(checkpoint)
 
 
 def text_split(input=""):
+    checkpoint = "unikei/t5-base-split-and-rephrase"
+    tokenizer = T5Tokenizer.from_pretrained(checkpoint)
+    model = T5ForConditionalGeneration.from_pretrained(checkpoint)
     complex_tokenized = tokenizer(
         input,
         padding="max_length",
@@ -39,20 +45,24 @@ def text_split(input=""):
         max_length=256,
         return_tensors="pt",
     )
-
+    print("error1")
+    print("---------------------")
+    # print(dir(model))
     simple_tokenized = model.generate(
         complex_tokenized["input_ids"],
         attention_mask=complex_tokenized["attention_mask"],
         max_length=256,
         num_beams=5,
     )
+    print("error2")
     simple_sentences = tokenizer.batch_decode(
         simple_tokenized, skip_special_tokens=True
     )
     sentences = [s.strip() for s in simple_sentences[0].split(".") if s.strip()]
-    print(sentences)
+    return sentences
 
-
+output = text_split("She did not cheat on the test, for it was the wrong thing to do.")
+# print(output)
 # --------------------------------------------------------------------------
 # -------------------------- Text Comparator ------------------------------
 # --------------------------------------------------------------------------
@@ -62,70 +72,78 @@ from sklearn.metrics.pairwise import cosine_similarity
 
 sentences = ["That is a happy person", "That is a very happy person"]
 
-model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
-
 
 def text_comparator(sentence_1, sentence_2):
-    embeddings = model.encode([sentence_1, sentence_2])
+    print("new error 1")
+    
+    sentences = [sentence_1, sentence_2]
+
+    model = SentenceTransformer('sentence-transformers/all-mpnet-base-v2')
+    embeddings = model.encode(sentences)
+    print("new error 2")
     similarity = cosine_similarity([embeddings[0]], [embeddings[1]])
     print(similarity[0][0])
+    return similarity[0][0]
 
+output = text_comparator("I love her", "She loves him")
+# print(output)
 
 # --------------------------------------------------------------------------
 # -------------------------- Multi Labeling ------------------------------
 # --------------------------------------------------------------------------
 
-import numpy as np
-import tensorflow as tf
-import random
-from tensorflow.keras.models import load_model 
-import nltk
-from nltk.stem.lancaster import LancasterStemmer
-import json
-import pandas as pd
-import keras
-import pickle
+# import numpy as np
+# import tensorflow as tf
+# import random
+# from tensorflow.keras.models import load_model 
+# import nltk
+# from nltk.stem.lancaster import LancasterStemmer
+# import json
+# import pandas as pd
+# import keras
+# import pickle
 
-stemmer = LancasterStemmer()
+# stemmer = LancasterStemmer()
 
-with open("labeling_models/words.pkl", "rb") as words_file:
-    words = pickle.load(words_file)
-# words = pd.read_pickle(r'labeling_models/words.pkl')
+# with open("labeling_models/words.pkl", "rb") as words_file:
+#     words = pickle.load(words_file)
+# # words = pd.read_pickle(r'labeling_models/words.pkl')
 
-with open("labeling_models/classes.pkl", "rb") as classes_file:
-    classes = pickle.load(classes_file)
-model = load_model('new_model/model.h5')
-# with open("new_model/model.h5", "rb") as model_file:
-#     model = pickle.load(model_file)
-# model = tf.keras.models.load_model('new_model/my_model.keras')
+# with open("labeling_models/classes.pkl", "rb") as classes_file:
+#     classes = pickle.load(classes_file)
+# model = load_model('new_model/model.h5')
+# # with open("new_model/model.h5", "rb") as model_file:
+# #     model = pickle.load(model_file)
+# # model = tf.keras.models.load_model('new_model/my_model.keras')
+# def response(user_input):
+#     user_input_words = nltk.word_tokenize(user_input)
+#     user_input_words = [stemmer.stem(word.lower()) for word in user_input_words]
+#     input_bag = [0] * len(words)
 
-def response(user_input):
-    user_input_words = nltk.word_tokenize(user_input)
-    user_input_words = [stemmer.stem(word.lower()) for word in user_input_words]
-    input_bag = [0] * len(words)
+#     for s in user_input_words:
+#         for i, w in enumerate(words):
+#             if w == s:
+#                 input_bag[i] = 1
 
-    for s in user_input_words:
-        for i, w in enumerate(words):
-            if w == s:
-                input_bag[i] = 1
+#     input_bag = np.array(input_bag).reshape(1, -1)
 
-    input_bag = np.array(input_bag).reshape(1, -1)
+#     results = model.predict(input_bag)
 
-    results = model.predict(input_bag)
+#     result_index = np.argmax(results)
+#     tag = classes[result_index]
 
-    result_index = np.argmax(results)
-    tag = classes[result_index]
+#     with open("./dataset.json") as json_data:
+#         intents = json.load(json_data)
 
-    with open("./dataset.json") as json_data:
-        intents = json.load(json_data)
+#     for intent in intents["intents"]:
+#         if intent["tag"] == tag:
+#             response = random.choice(intent["responses"])
+#             break
 
-    for intent in intents["intents"]:
-        if intent["tag"] == tag:
-            response = random.choice(intent["responses"])
-            break
+#     return response
 
-    return response
-
+# output = response("The dosa was tasty")
+# print(output)
 
 # response(
 #     "All around lovely brunch spot! Cute and quaint decor, very friendly and accommodating host and wait staff, delicious coffee, BYOB and endless food options. It was very difficult to choose one thing, but the combinations are unique and flavorful- far from routine. The meals are reasonably portioned and packed with flavor. I appreciate the time out into each dish as opposed to hugeee portions of hastily made eggs or pancakes. There are so many elements to the dishes. I had the gypsy eggs! Will have took back to try other options! Expect to wait to be seated and don't go with a very large group as seating will be difficult- in my mind it's worth the wait and the wait shows that many others feel the same! Have heard from multiple sources that this was a spot not to miss and I 100% concur. Thank you for great food, friendly atmosphere and BYOB"
@@ -135,43 +153,106 @@ def response(user_input):
 # -------------------------- Sentimental Analysis ------------------------
 # --------------------------------------------------------------------------
 
-from transformers import AutoModelForSequenceClassification
-from transformers import TFAutoModelForSequenceClassification
-from transformers import AutoTokenizer, AutoConfig
-import numpy as np
-from scipy.special import softmax
+# from transformers import AutoModelForSequenceClassification
+# from transformers import TFAutoModelForSequenceClassification
+# from transformers import AutoTokenizer, AutoConfig
+# import numpy as np
+# from scipy.special import softmax
 
-def preprocess(text):
-    new_text = []
-    for t in text.split(" "):
-        t = '@user' if t.startswith('@') and len(t) > 1 else t
-        t = 'http' if t.startswith('http') else t
-        new_text.append(t)
-    return " ".join(new_text)
-MODEL = f"cardiffnlp/twitter-roberta-base-sentiment-latest"
-tokenizer = AutoTokenizer.from_pretrained(MODEL)
-config = AutoConfig.from_pretrained(MODEL)
+# def preprocess(text):
+#     new_text = []
+#     for t in text.split(" "):
+#         t = '@user' if t.startswith('@') and len(t) > 1 else t
+#         t = 'http' if t.startswith('http') else t
+#         new_text.append(t)
+#     return " ".join(new_text)
+# MODEL = f"cardiffnlp/twitter-roberta-base-sentiment-latest"
+# tokenizer = AutoTokenizer.from_pretrained(MODEL)
+# config = AutoConfig.from_pretrained(MODEL)
 
-model = AutoModelForSequenceClassification.from_pretrained(MODEL)
+# model = AutoModelForSequenceClassification.from_pretrained(MODEL)
+
+# def sentimental_analysis(text):
+#     text = preprocess(text)
+#     encoded_input = tokenizer(text, return_tensors='pt')
+#     output = model(**encoded_input)
+#     scores = output[0][0].detach().numpy()
+#     scores = softmax(scores)
+#     ranking = np.argsort(scores)
+#     ranking = ranking[::-1]
+#     highest_score = -np.inf
+#     highest_label = None
+
+#     for i in range(scores.shape[0]):
+#         l = config.id2label[ranking[i]]
+#         s = scores[ranking[i]]
+#         rounded_score = np.round(float(s), 4)
+#         if rounded_score > highest_score:
+#             highest_score = rounded_score
+#             highest_label = l
+
+#     return highest_label
+from pysentimiento import create_analyzer
 def sentimental_analysis(text):
-  
-  text = preprocess(text)
-  encoded_input = tokenizer(text, return_tensors='pt')
-  output = model(**encoded_input)
-  scores = output[0][0].detach().numpy()
-  scores = softmax(scores)
-  ranking = np.argsort(scores)
-  ranking = ranking[::-1]
-  highest_score = -np.inf
-  highest_label = None
-
-  for i in range(scores.shape[0]):
-      l = config.id2label[ranking[i]]
-      s = scores[ranking[i]]
-      rounded_score = np.round(float(s), 4)
-      if rounded_score > highest_score:
-          highest_score = rounded_score
-          highest_label = l
-
-  return highest_label
+    analyzer = create_analyzer(task="sentiment", lang="en")
+    output = analyzer.predict(text)
+    highest_prob_sentiment = max(output.probas, key=output.probas.get)
+    return highest_prob_sentiment
 # sentimental_analysis("I am happy")
+output = sentimental_analysis("The waiter took long time to serve")
+# print(output)
+
+
+# ///////////////////////////////////////////////////////////////////////////////////
+import numpy as np
+import tensorflow as tf
+import random
+import nltk
+from nltk.stem.lancaster import LancasterStemmer
+import pickle
+import warnings
+from tensorflow.keras.models import load_model 
+import pandas as pd
+from datasets import load_dataset
+
+stemmer = LancasterStemmer()
+
+# model = tf.keras.models.load_model("my_model.keras")
+
+# with open("classes.h5", "rb") as classes_file:
+#     classes = pickle.load(classes_file)
+
+# with open("words.h5", "rb") as words_file:
+#     words = pickle.load(words_file)
+
+with open("labeling_models/words.pkl", "rb") as words_file:
+    words = pickle.load(words_file)
+# words = pd.read_pickle(r'labeling_models/words.pkl')
+
+with open("labeling_models/classes.pkl", "rb") as classes_file:
+    classes = pickle.load(classes_file)
+model = load_model('new_model/model.h5')
+
+def preprocess_input(input_text):
+    input_words = nltk.word_tokenize(input_text)
+    input_words = [stemmer.stem(word.lower()) for word in input_words]
+    input_bag = [0] * len(words)
+
+    for s in input_words:
+        for i, w in enumerate(words):
+            if w == s:
+                input_bag[i] = 1
+
+    input_bag = np.array(input_bag).reshape(1, -1)
+    return input_bag
+
+def response(input_text):
+    input_bag = preprocess_input(input_text)
+    predictions = model.predict(input_bag)
+    result_index = np.argmax(predictions)
+    predicted_class = classes[result_index]
+    return predicted_class
+
+input_text = "The service at this restaurant was amazing!"
+predicted_class = response(input_text)
+# print(predicted_class)
